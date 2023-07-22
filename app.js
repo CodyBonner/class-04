0306acc89af0a2cce0f8da25990c74c637fd7c85
Cody Bonner <bonner_cody94@yahoo.com>
2023-07-22 21:18:32 +0000

    Finished loops and restructered functions

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 'use strict'
 
-let username = prompt("Hi whats your name?");
+function greeting(){
+    
+    let userName = prompt("Hello! What is your name?");
+    let user1 = userName;
+    
+    confirm("Is your name: " +user1);
+    
+    alert("Welcome " + user1);
 
-
-let user1 = confirm("Is your name? " + username);
-
-if(username == null){
-    username = prompt("Please write your name");
-
+    document.write("Welcome:" + user1);
 }
-else if (user1 == true){
-    alert("Welcome " + username);
-}
+
+
+function sleepinessLevel(){
+    let sLevel = prompt("What is your sleepiness level?");
+    let stLevel = parseInt(sLevel);
+    
+    for(let i = 0; i<stLevel; i=i+1){
+        document.write("😴")
+    }
+
+}    
+
 
  function userBenefit(userResponse, userResponse2) {
 
     userResponse = prompt("What benefit do you receive from streaming?");
     userResponse2 = prompt("Is this a good or a bad benefit?");
-    let finalResponse =  userResponse + " Benefit Type " +userResponse2;
+    let finalResponse =  userResponse + " Benefit Type: " +userResponse2;
     return finalResponse
     
 
 }
+
+//Future function for image incorporation
+//function rating(){
+   // let userInput = prompt("Enter a number between 1 and 10");
+    //let userI = parseInt(userInput);
+    //if(userI<= 10){
+      //      alert("Valid Result!")
+        //    for(let i =0; userI<=10; i=i+1){
+
+          //      document.write
+           // }
+   // }
+     //   else{
+       //     alert("Invalid Result")
+
+        //}
+        
+    //}     
+
+
+
+
 
 
 
@@ -29,5 +62,5 @@ else if (user1 == true){
    // alert("Please confirm your name!");
  //   username = prompt("Please write your name");
 //}
-document.write("Hi, " + username);
+//document.write("Hi, " + username);
 
